b8fff4316aee3699c8946b1645f39afb1f6fe73c
lizx <lizx@ihczd.com>
2016-01-18 15:47:41 +0800

    2016-01-18 lizx 修改配置，服务器可成功启动
    Change-Id: Idfe058fd7f40ea0f8a5bd9d617ba58f1e62aa030
    Signed-off-by: lizx <lizx@ihczd.com>

--- src/test/java/com/admin/junit/JunitTest.java
+++ src/test/java/com/admin/junit/JunitTest.java
@@ -1,13 +1,34 @@
 package com.admin.junit;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.admin.authority.model.User;
+import com.admin.authority.service.IUserService;
+
+
 /**
  * 
  * @author lzx
  * @version 1.0
  * @create_date 下午6:45:45
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/spring/app-*.xml")
 public class JunitTest {
 
-	public static void main(String[] args) {
+	@Autowired
+	private IUserService userService;
+	
+	@Test
+	public void userTest(){
+		//userService.getUserByName("lzx");
+		User user = userService.get(1);
+		System.out.println(user.getUsername());
+		
 	}
+
 }
