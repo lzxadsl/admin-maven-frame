44f10f152094fc5b57d8755d64fd07a6297df005
lizx <lizx@ihczd.com>
2016-01-29 17:31:17 +0800

    2016-01-29 lizx 多线程测试
    Change-Id: Ieaf44d96fbd9c24c3e6f98b3049b6e28339aa0e7
    Signed-off-by: lizx <lizx@ihczd.com>

--- src/test/java/com/admin/junit/MainTest.java
+++ src/test/java/com/admin/junit/MainTest.java
@@ -1,5 +1,8 @@
 package com.admin.junit;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import org.apache.commons.io.FilenameUtils;
 
 /**
@@ -10,9 +13,17 @@ import org.apache.commons.io.FilenameUtils;
  */
 public class MainTest {
 
+	private int x = 0;
+	Lock lock = new ReentrantLock();
 	public static void main(String[] args) {
 		String str = "brrow.zip";
 		String extension = FilenameUtils.getExtension(str);
 		System.out.println(extension);
+	}
+	
+	public void add(){
+		
+		x++;
+		System.out.println("结果："+x);
 	}
 }
